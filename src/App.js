671dd99dcbaab152f9143e1f93ab457b9635ca42
quanghavan29 <quanghavan29@gmail.com>
2021-12-12 15:10:38 +0700

    chia route cho page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Header from './components/Home/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      Task Management
-    </div>
+    <BrowserRouter>
+    <Header />
+      <Switch>
+        <Route exact path='/home' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/contact' component={Contact} />
+        <Route exact path='/' component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
